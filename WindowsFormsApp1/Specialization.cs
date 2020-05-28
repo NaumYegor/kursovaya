4b0e6b9cf4d18c3da00a7d65bf85e2c64b7b559b
Yehor <yehornaumenko@gmail.com>
2020-05-28 16:43:16 +0300

    Added Specialization class

---
+++ WindowsFormsApp1/Specialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Specialization
+    {
+        public Dictionary<string, string> Info { get; private set; }
+
+        public Specialization()
+        {
+            Info = new Dictionary<string, string>();
+            Info.Add("City", "");
+            Info.Add("School", "");
+            Info.Add("Faculty", "");
+            Info.Add("Specialization", "");
+            Info.Add("Type", "");
+            Info.Add("Price", "");
+        }
+
+        public void SetData(string name, string data)
+        {
+            if (!Info.ContainsKey("name"))
+                throw new Exception("This key does not exist.");
+            Info[name] = data;
+            return;
+        }
+    }
+}
